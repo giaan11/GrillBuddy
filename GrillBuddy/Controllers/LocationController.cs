76bba8660492e59a82f1c3425b3cad225c42daa5
mattia.montani <mattia.montani@studenti.itsincom.it>
2021-06-17 12:02:49 +0200

    Created location controller

--- GrillBuddy/Controllers/LocationController.cs
+++ GrillBuddy/Controllers/LocationController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GrillBuddy.DTO;
 
 namespace GrillBuddy.Controllers
 {
@@ -19,7 +21,79 @@ namespace GrillBuddy.Controllers
         {
             _logger = logger;
         }
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult GetAllLocations()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch(NotImplementedException e)
+            {
+                return NotFound();
+            }
+            
+        }
 
+        [HttpGet]
+        [Route("GetSingleById{id:int}")]
+        public IActionResult GetSingleById(int id)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (NotImplementedException e)
+            {
+                return NotFound();
+                //_logger.LogError("" + e);
+            }
+        }
+
+
+        [HttpPost]
+        [Route("CreateNew")]
+        public IActionResult Create(LocationDTO location)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (NotImplementedException e)
+            {
+                return NotFound();
+                //_logger.LogError(""+e);
+            }
+        }
+        [HttpPut]
+        [Route("Update")]
+        public IActionResult Update(LocationDTO newLocation)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (NotImplementedException e)
+            {
+                return NotFound();
+                //_logger.LogError("" + e);
+            }
+        }
+        [HttpDelete]
+        [Route("RemoveLocation")]
+        public IActionResult Remove(LocationDTO toDelete)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (NotImplementedException e)
+            {
+                return NotFound();
+                //_logger.LogError("" + e);
+            }
+        }
 
     }
 }
