f59a641dd472aaaaae77ce0933c31f4b3e81062d
mattia.montani <mattia.montani@studenti.itsincom.it>
2021-06-17 11:27:58 +0200

    Location DTO created

---
+++ GrillBuddy/Controllers/LocationController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrillBuddy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+        private readonly ILogger<LocationController> _logger;
+     
+
+        public LocationController(ILogger<LocationController> logger)
+        {
+            _logger = logger;
+        }
+
+
+    }
+}
---
+++ GrillBuddy/DTO/LocationDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace GrillBuddy.DTO
+{
+    public class LocationDTO
+    {
+        public int LocationId { get; set; }
+        public string LocationName { get; set; }
+        public string LocationAddress { get; set; }
+        //public string LocationLatitude { get; set; }
+        //public string LocationLongitude { get; set; }
+    }
+}
